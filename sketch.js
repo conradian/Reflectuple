a138b4178ed78962d5e73c68aad8319ef0e568a8
conradian <conrad.grindheim-borrell@durham.ac.uk>
2018-12-22 20:03:53 +0000

    Added scrambles and answer key

--- sketch.js
+++ sketch.js
@@ -12,11 +12,11 @@ var cellNumbx = 9;
 var cellNumby = 9;
 
 //Where the grid starts and ends (horizontally)
-var xgridstart = w*0.22;
+var xgridstart = w*0.1;
 var xgridend = xgridstart+cellNumbx*cellx;
 
 //Where the grid starts and ends (vertically)
-var ygridstart = h*0.3;
+var ygridstart = h*0.2;
 var ygridend = ygridstart+cellNumby*celly;
 
 //Array with all of the colors
@@ -26,7 +26,7 @@ var colors = ["#9D2C38","#E43D46","#FB912F","#FFE662","#63C44E","#357347","#1F3F
 //-1 is the default meaning nothing has been selected
 var tempSelect = -1;
 
-//Grid that stores all of the data regarding whether there is an event or not
+//Grid that stores all of the cell colors
 var grid = new Array(cellNumby);
 for(i = 0; i < cellNumby; i++)
 {
@@ -36,11 +36,34 @@ for(i = 0; i < cellNumby; i++)
     grid[i][j] = colors[i];
   }
 }
-console.log(grid);
+
+//-----------------------------------Stats--------------------------------------
+
+//Score variables
+var moves = 0;
+var time = 0;
+var timeAtStart;
 
 //Setup
 function setup()
 {
+  //Button that runs scramble function (easy)
+  scrambleBtEasy = createButton("Easy Scramble");
+  scrambleBtEasy.class("button");
+  scrambleBtEasy.position(w*0.1, h*0.8+scrambleBtEasy.height*0.5-2);
+  scrambleBtEasy.mousePressed(scrambleEasy);
+
+  //Button that runs scramble function (normal)
+  scrambleBtNormal = createButton("Normal Scramble");
+  scrambleBtNormal.class("button");
+  scrambleBtNormal.position(w*0.2, h*0.8+scrambleBtNormal.height*0.5-2);
+  scrambleBtNormal.mousePressed(scrambleNormal);
+
+  //Button that runs scramble function (hard)
+  scrambleBtHard = createButton("Hard Scramble");
+  scrambleBtHard.class("button");
+  scrambleBtHard.position(w*0.315, h*0.8+scrambleBtHard.height*0.5-2);
+  scrambleBtHard.mousePressed(scrambleHard);
 
 }
 
@@ -54,9 +77,9 @@ function draw()
   textFont("Verdana");
   fill("#FFFFFF");
 
-  //----------------------------------Drawing-----------------------------------
+  //-----------------------------Drawing Grid-----------------------------------
   //Draw horizontal lines
-  for(i = 0; i < cellNumby+1;i++)
+  for(i = 0; i < cellNumby+1; i++)
   {
   	line(xgridstart,ygridstart+i*celly,xgridend,ygridstart+i*celly);
   }
@@ -78,6 +101,27 @@ function draw()
   	}
   }
 
+  //----------------------------Drawing Answer Key------------------------------
+  //Draw horizontal lines
+  for(i = 0; i < cellNumby + 1; i++)
+  {
+  	line(xgridend + cellx*2,ygridstart+i*celly,xgridend + cellx*3,ygridstart+i*celly);
+  }
+
+  //Draw vertical lines
+  for(i = 0; i < 2; i++)
+  {
+  	line(xgridend + cellx*2 + i*cellx, ygridstart, xgridend + cellx*2 + i*cellx, ygridend);
+  }
+
+  //Filling in the squares
+  for(i = 0; i < colors.length; i++)
+  {
+    fill(colors[i]);
+    rect(xgridend + cellx*2,ygridstart+i*celly,cellx,celly);
+  }
+
+
 }
 
 //------------------------------------Functions---------------------------------
@@ -97,7 +141,7 @@ function mousePressed()
     }
     else
     {
-      swap(grid, tempSelect, [gridx, gridy]);
+      mirror(grid, tempSelect, [gridx, gridy]);
       tempSelect = -1;
     }
   }
@@ -119,13 +163,17 @@ function clamp(n, a, b)
   return(n);
 }
 
-//Carries out the swapping operation
-function swap(array, coorA, coorB)
+//Carries out the mirroring operation
+function mirror(array, coorA, coorB)
 {
+
+  //All temporary manipulation will be handled in this array (reflection).
   var array = [];
 
   if(coorA[0] == coorB[0])
   {
+    //Since this function only works when you select the left/topmost cell at
+    //First, if this isn't the case then swap them.
     if( (coorB[1] < coorA[1]) )
     {
       var temp = coorB[1];
@@ -140,7 +188,7 @@ function swap(array, coorA, coorB)
 
     array.reverse();
 
-    for(i = 0; i < abs(coorA[1] - coorB[1]) + 1; i++)
+    for(i = 0; i < Math.abs(coorA[1] - coorB[1]) + 1; i++)
     {
       grid[coorA[1]+i][coorA[0]] = array[i];
     }
@@ -148,6 +196,8 @@ function swap(array, coorA, coorB)
   }
   else if(coorA[1] == coorB[1])
   {
+    //Since this function only works when you select the left/topmost cell at
+    //First, if this isn't the case then swap them.
     if( (coorB[0] < coorA[0]) )
     {
       var temp = coorB[0];
@@ -155,16 +205,63 @@ function swap(array, coorA, coorB)
       coorA[0] = temp;
     }
 
-    for(i = 0; i < abs(coorA[0] - coorB[0]) + 1; i++)
+    for(i = 0; i < Math.abs(coorA[0] - coorB[0]) + 1; i++)
     {
       array.push(grid[coorA[1]][coorA[0]+i]);
     }
 
     array.reverse();
 
-    for(i = 0; i < abs(coorA[0] - coorB[0]) + 1; i++)
+    for(i = 0; i < Math.abs(coorA[0] - coorB[0]) + 1; i++)
     {
       grid[coorA[1]][coorA[0]+i] = array[i];
     }
   }
+}
+
+
+//Scrambles the reflectuple
+function scrambleEasy()
+{
+
+  var array = grid;
+
+  for(i = 0; i < 20; i++)
+  {
+    mirror(array, [randomRange(0,cellNumbx-1),randomRange(0,cellNumby-1)], [randomRange(0,cellNumbx-1),randomRange(0,cellNumby-1)]);
+  }
+
+}
+
+function scrambleNormal()
+{
+
+  var array = grid;
+
+  for(i = 0; i < 40; i++)
+  {
+    mirror(array, [randomRange(0,cellNumbx-1),randomRange(0,cellNumby-1)], [randomRange(0,cellNumbx-1),randomRange(0,cellNumby-1)]);
+  }
+
+}
+
+function scrambleHard()
+{
+
+  var array = grid;
+
+  for(i = 0; i < 60; i++)
+  {
+    mirror(array, [randomRange(0,cellNumbx-1),randomRange(0,cellNumby-1)], [randomRange(0,cellNumbx-1),randomRange(0,cellNumby-1)]);
+  }
+
+}
+
+//Random range function
+//https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+function randomRange(min, max)
+{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
